0362bc3a7f923c9cd0b20ab84819eded63015fee
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-05-25 10:29:36 -0700

    created a random cookie jar

--- server.js
+++ server.js
@@ -2,11 +2,30 @@ const express = require("express");
 const app = express();
 const birds = require("./birds");
 const PORT = 3000;
+const cookieParser = require("cookie-parser");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(cookieParser());
+
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  domain: "localhost",
+  secure: false,
+  path: "/",
+  // 30 days
+  expires: new Date(Date.now() + 60 * 60 * 24 * 1000 * 30),
+};
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/get-cookie", (req, res) => {
+  const token = `${Math.random() * 100}`;
+
+  res.cookie("random-cookie-token", token, COOKIE_OPTIONS);
+  res.send("have a cookie from the cookie jar");
+});
 
 app.use("/birds", birds);
